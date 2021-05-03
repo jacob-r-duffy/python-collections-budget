cdc387a0e7f582ee4e44912e62a873a15484e0b8
Jacob Duffy <jacob.duffy@pilottravelcenters.com>
2021-05-03 15:44:32 -0400

    module 4 commit

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -7,7 +7,7 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = expenses.categorize_set_comprehension()
-    if divided_set_comp != divided_for_loop:
+    if not divided_set_comp == divided_for_loop:
         'Set are NOT equal by == test'
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
